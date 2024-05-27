882e1d259d3ade7e767ff10169b5987f761c1782
Ahmednet380 <Ahmednet380@gmail.com>
2024-05-27 11:39:22 +0300

    establishes a foundational `Date` class with robust validation logic for handling date-related operations.

---
+++ OOPConcepts/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using OOP.Logic;
+
+try
+{
+	var date1 = new Date { Year =2025, Month = 5, Day = 50 };
+	Console.WriteLine(date1);
+}
+catch (Exception Ex)
+{
+
+	Console.WriteLine(Ex.Message);
+}
